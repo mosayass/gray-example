34db4555a500ac6a9fa4bf50fc5b5fb6ba0804f9
Mosa Yasin <mosayass223@gmail.com>
2026-01-22 14:48:04 +0300

    change name of class

--- src/executors/GrayExample.py
+++ src/executors/GrayExample.py
@@ -15,7 +15,7 @@ from components.GrayExample.src.utils.response import build_response
 from components.GrayExample.src.models.PackageModel import PackageModel
 
 
-class Package(Component):
+class GrayExample(Component):
     def __init__(self, request, bootstrap):
         super().__init__(request, bootstrap)
         self.request.model = PackageModel(**(self.request.data))
