93b0becc85437293be9e9ff39b3c8e3da83d485f
Mosa Yasin <mosayass223@gmail.com>
2026-01-26 13:01:22 +0300

    fix wrapping error

--- src/executors/MixerExample.py
+++ src/executors/MixerExample.py
@@ -10,7 +10,7 @@ from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
 from components.GrayExample.src.utils.response import build_response_mixer
 from components.GrayExample.src.models.PackageModel import PackageModel
-
+import copy
 
 class MixerExample(Component):
     def __init__(self, request, bootstrap):
@@ -41,18 +41,19 @@ class MixerExample(Component):
         img1 = Image.get_frame(img=self.image_main_id, redis_db=self.redis_db)
         img2 = Image.get_frame(img=self.image_overlay_id, redis_db=self.redis_db)
 
-        # Ensure sizes match for blending (Resize img2 to match img1)
         h, w = img1.value.shape[:2]
         img2_resized = cv2.resize(img2.value, (w, h))
+        mixed_array = cv2.addWeighted(img1.value, 1 - self.alpha, img2_resized, self.alpha, 0)
+        diff_array = cv2.absdiff(img1.value, img2_resized)
 
+        wrapper_mix = copy.deepcopy(img1)  # Create a clone of the wrapper
+        wrapper_mix.value = mixed_array  # Stuff the new data inside
 
-        mixed = cv2.addWeighted(img1.value, 1 - self.alpha, img2_resized, self.alpha, 0)
+        wrapper_diff = copy.deepcopy(img1)  # Create another clone
+        wrapper_diff.value = diff_array
 
-        diff = cv2.absdiff(img1.value, img2_resized)
-
-
-        self.image_mix = Image.set_frame(img=mixed, package_uID=self.uID, redis_db=self.redis_db)
-        self.image_diff = Image.set_frame(img=diff, package_uID=self.uID, redis_db=self.redis_db)
+        self.image_mix = Image.set_frame(img=wrapper_mix, package_uID=self.uID, redis_db=self.redis_db)
+        self.image_diff = Image.set_frame(img=wrapper_diff, package_uID=self.uID, redis_db=self.redis_db)
 
 
         return build_response_mixer(context=self)
